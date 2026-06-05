c87d38954618e42c37422a50ac41cca0dc089811
valecristiano <valeriamdcristiano@gmail.com>
2026-06-05 17:56:50 +0300

    struttura di base

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,8 @@
+
+<figure class="comic-img-figure col-4 col-lg-2">
+      <div class="comic-img-main">
+        <img src="{{$src}}" alt="{{$alt}}" />
+      </div>
+      <figcaption> {{$slot}} </figcaption>
+</figure>
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends("layouts.layout")
+
+@section('title', 'Homepage')
+
+@section("content")
+<h1>Contenuto</h1>
+<x-card src="" alt=""> Titolo della Card </x-card> 
+@endsection
